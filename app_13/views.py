2e760e27d51604063ab9a7e7e89023069b732754
13-agu <shinicito@gmail.com>
2018-07-16 17:31:50 +0900

    konnnakanzi

--- app_13/views.py
+++ app_13/views.py
@@ -1,5 +1,31 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render
+from requests_oauthlib import OAuth1Session
+import requests   # Web からデータを取ってくる時に使う
+import bs4        # スクレイピング
+import json
+from twitter import *
+
+AT='825188365007872000-af18jBAUI5BOkiqTWrbpX28WhKA0CrL'
+AS='y3ZSkUiMK60t5wmLoTcNsyMjw7sj4oHJS2kuvwfOeQj5s'
+CK='jqbmCibEoOjahpBVBMuWG0km6'
+CS='WKgRlgCuDl80bMbY1YwozZdztIt9mceahBNX2pnU3f9wqqy0zy'
+twitter = OAuth1Session(CK, CS, AT, AS)
 
 def appmain(request):
-	# write your python code!!
-	return HttpResponse('hello hogehoge')
+	url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+
+	params ={'count' : 1}
+	req = twitter.get(url, params = params)
+
+	if req.status_code == 200:
+		timeline = json.loads(req.text)
+		for tweet in timeline:
+			#print(tweet['text'])
+			#print('----------------------------------------------------')
+			s=tweet['text']
+	else:
+		print("ERROR: %d" % req.status_code)
+    
+    return render(request, '.html', {
+        'tw_1' : s,
+    })
